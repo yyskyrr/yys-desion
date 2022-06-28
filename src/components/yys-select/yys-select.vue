c043c52272a997cfa52afd16480bf1fc055a00cd
闫雅帅 <1165697552@qq.com>
2022-06-28 23:06:31 +0800

    update: select

--- src/components/yys-select/yys-select.vue
+++ src/components/yys-select/yys-select.vue
@@ -2,7 +2,7 @@
   <div :class="{'yys-select-disabled' : disabled}" class="yys-select">
     <div v-if="isTags">
       <div class="yys-tags-list" @click="onClick">
-        <YTextarea v-model="tagsList.join('--------------')" :placeholder="placeholder" auto-size @blur="onblur"
+        <YTextarea v-model="tagsList.join('--------------')" :placeholder="newPlaceholder" auto-size @blur="onblur"
         ></YTextarea>
         <div class="yys-tags-box">
           <div v-for="item in tagsList" class="yys-tags-item">{{ item }}
@@ -13,8 +13,10 @@
 
       <div v-show="isFocus" class="yys-tags-options-box" @mouseleave="isFocus=false"
       >
+
         <div
             v-for="(item, index) in optionsList"
+            v-if="optionsList.length > 0"
             :key="index"
             :class="{
           'yys-tags-selected': tagsList.indexOf(item.value) > -1,
@@ -24,18 +26,36 @@
         >
           {{ item.label }}
         </div>
+        <div v-if="optionsList.length === 0" class="yys-empty-option">
+          <svg height="41" viewBox="0 0 64 41" width="64" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fillRule="evenodd" transform="translate(0 1)">
+              <ellipse cx="32" cy="33" fill="#F5F5F5" rx="32" ry="7"></ellipse>
+              <g fillRule="nonzero" stroke="#D9D9D9">
+                <path
+                    d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"></path>
+                <path
+                    d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+                    fill="#FAFAFA"></path>
+              </g>
+            </g>
+          </svg>
+          {{ emptyText || '暂无数据' }}
+        </div>
       </div>
     </div>
 
     <div v-else>
       <div class="yys-select-box" @click="onClick">
-        <YInput v-if="loading" v-model="label" :disabled="disabled"
+        <YInput v-if="loading" v-model="label" :disabled="disabled" :size="size"
                 loading @blur="onblur">
         </YInput>
 
-        <YInput v-else v-model="label" :disabled="disabled" @blur="onblur">
+        <YInput v-else v-model="label" :disabled="disabled" :size="size" @blur="onblur">
           <template #suffix>
-            <i aria-hidden="true" class="fa fa-angle-down"></i>
+            <slot name="suffixIcon">
+              <i v-if="showArrow" aria-hidden="true" class="fa fa-angle-down"></i>
+            </slot>
+
           </template>
         </YInput>
       </div>
@@ -43,6 +63,7 @@
       <div v-show="isFocus" class="yys-option-box">
         <div
             v-for="(item, index) in optionsList"
+            v-if="optionsList.length > 0"
             :key="index"
             :class="{
           'yys-option-selected': currentValue == item.value,
@@ -51,6 +72,21 @@
             @mousedown="onSelect(item)"
         >
           {{ item.label }}
+        </div>
+        <div v-if="optionsList.length === 0" class="yys-empty-option">
+          <svg height="41" viewBox="0 0 64 41" width="64" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fillRule="evenodd" transform="translate(0 1)">
+              <ellipse cx="32" cy="33" fill="#F5F5F5" rx="32" ry="7"></ellipse>
+              <g fillRule="nonzero" stroke="#D9D9D9">
+                <path
+                    d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"></path>
+                <path
+                    d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+                    fill="#FAFAFA"></path>
+              </g>
+            </g>
+          </svg>
+          {{ emptyText || '暂无数据' }}
         </div>
       </div>
     </div>
@@ -69,14 +105,14 @@ export default {
   },
   data() {
     return {
-      isFocus: false,
+      isFocus: this.defaultOpen || this.open,
       isTagsSelect: false,
       isTagsFocus: false,
       optionsList: [],
       tagsList: [],
       label: "",
-      currentValue: this.value,
-      default: this.$slots.default,
+      currentValue: this.value || this.defaultValue,
+      newPlaceholder: this.placeholder,
     };
   },
   watch: {
@@ -86,18 +122,24 @@ export default {
         this.optionsList = []
         this.getOptions()
       }
-    }
+    },
   },
   computed: {
     isTags() {
-      return this.mode === 'tags'
+      return this.mode === 'tags' || this.mode === 'multiple'
     },
   },
   props: {
-    value: {type: String || Number, default: ""},
+    value: {default: ""},
+    defaultValue: {default: ""},
     mode: String,
     placeholder: String,
+    emptyText: String,
+    size: String,
     disabled: Boolean,
+    open: Boolean,
+    defaultOpen: Boolean,
+    showArrow: {type: Boolean, default: true},
     labelInValue: Boolean,
     loading: Boolean
   },
@@ -128,10 +170,9 @@ export default {
       this.$emit("change", item.value);
     },
     getOptions() {
-      console.log('this.value', this.value)
       this.$slots.default.forEach((item) => {
-        if (this.value === item.data.attrs?.value || item.data.key && this.mode !== 'tags') {
-          this.label = this.value;
+        if (this.value || this.defaultValue === item.data.attrs?.value || item.data.key && this.mode !== 'tags') {
+          this.label = this.value || this.defaultValue;
         }
         this.optionsList.push({
           label: item.children[0].text,
@@ -140,15 +181,22 @@ export default {
       });
     },
     onTagsSelect(item) {
+      this.isTagsSelect = true
+      if (this.mode === 'multiple') {
+        this.optionsList.splice(this.optionsList.indexOf(item), 1)
+        this.tagsList.push(item.value);
+        this.$emit("change", this.tagsList);
+        return
+      }
       if (this.tagsList.indexOf(item.value) > -1) {
         this.tagsList.splice(this.tagsList.indexOf(item.value), 1)
       } else {
         this.tagsList.push(item.value);
       }
       if (this.tagsList.length > 0) {
-        this.placeholder = ''
+        this.newPlaceholder = ''
       }
-      this.isTagsSelect = true
+
       this.$emit("change", this.tagsList);
     },
   },
--- src/pages/select.vue
+++ src/pages/select.vue
@@ -52,6 +52,161 @@
         {{ city }}
       </YOption>
     </YSelect>
+
+    <p>隐藏已选择选项</p>
+    <YSelect
+        :value="selectedItems"
+        mode="multiple"
+        placeholder="Inserted are removed"
+        style="width: 100%"
+        @change="handleChange4"
+    >
+      <YOption v-for="item in filteredOptions" :key="item" :value="item">
+        {{ item }}
+      </YOption>
+    </YSelect>
+
+    <p>三种大小</p>
+    <YSelect size="large" style="width: 200px" value="a1" @change="handleChange">
+      <YOption v-for="i in 25" :key="(i + 9).toString(36) + i">
+        {{ (i + 9).toString(36) + i }}
+      </YOption>
+    </YSelect>
+    <br><br>
+    <YSelect size="default" style="width: 200px" value="a1" @change="handleChange">
+      <YOption v-for="i in 25" :key="(i + 9).toString(36) + i">
+        {{ (i + 9).toString(36) + i }}
+      </YOption>
+    </YSelect>
+    <br><br>
+    <YSelect size="small" style="width: 200px" value="a1" @change="handleChange">
+      <YOption v-for="i in 25" :key="(i + 9).toString(36) + i">
+        {{ (i + 9).toString(36) + i }}
+      </YOption>
+    </YSelect>
+
+    <p>多选</p>
+    <YSelect mode="tags" placeholder="Please select" style="width: 100%" @change="handleChange">
+      <YOption v-for="i in 25" :key="(i + 9).toString(36) + i">
+        {{ (i + 9).toString(36) + i }}
+      </YOption>
+    </YSelect>
+
+    <p>后缀图标</p>
+    <YSelect default-value="lucy" style="width: 120px" @change="handleChange">
+      <i slot="suffixIcon" aria-hidden="true" class="fa fa-envelope-o"></i>
+      <YOption value="jack">
+        Jack
+      </YOption>
+      <YOption value="lucy">
+        Lucy
+      </YOption>
+      <YOption disabled value="disabled">
+        Disabled
+      </YOption>
+      <YOption value="Yiminghe">
+        yiminghe
+      </YOption>
+    </YSelect>
+    <YSelect default-value="lucy" disabled style="width: 120px">
+      <i slot="suffixIcon" aria-hidden="true" class="fa fa-envelope-o"></i>
+      <YOption value="lucy">
+        Lucy
+      </YOption>
+    </YSelect>
+    <br><br>
+    <table>
+      <thead>
+      <tr>
+        <th>参数</th>
+        <th>说明</th>
+        <th>类型</th>
+        <th>默认值</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>defaultValue</td>
+        <td>指定默认选中的条目</td>
+        <td>string|string[]|number|number[]</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>disabled</td>
+        <td>是否禁用</td>
+        <td>boolean</td>
+        <td>false</td>
+      </tr>
+      <tr>
+        <td>labelInValue</td>
+        <td>是否把每个选项的 label 包装到 value 中，会把 Select 的 value 类型从 <code>string</code> 变为 <code>{key: string, label:
+          vNodes}</code> 的格式
+        </td>
+        <td>boolean</td>
+        <td>false</td>
+      </tr>
+      <tr>
+        <td>mode</td>
+        <td>设置 Select 的模式为多选或标签</td>
+        <td> 'multiple' | 'tags'</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>notFoundContent</td>
+        <td>当下拉列表为空时显示的内容</td>
+        <td>string|slot</td>
+        <td>'Not Found'</td>
+      </tr>
+      <tr>
+        <td>placeholder</td>
+        <td>选择框默认文字</td>
+        <td>string|slot</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>showArrow</td>
+        <td>是否显示下拉小箭头</td>
+        <td>boolean</td>
+        <td>true</td>
+      </tr>
+      <tr>
+        <td>size</td>
+        <td>选择框大小，可选 <code>large</code> <code>small</code></td>
+        <td>string</td>
+        <td>default</td>
+      </tr>
+      <tr>
+        <td>suffixIcon</td>
+        <td>自定义的选择框后缀图标</td>
+        <td>VNode | slot</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>value(v-model)</td>
+        <td>指定当前选中的条目</td>
+        <td>string|string[]|number|number[]</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>emptyText</td>
+        <td>自定义选项为空时显示文字</td>
+        <td>string</td>
+        <td>暂无数据</td>
+      </tr>
+      <tr>
+        <td>defaultOpen</td>
+        <td>是否默认展开下拉菜单</td>
+        <td>boolean</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>open</td>
+        <td>是否展开下拉菜单</td>
+        <td>boolean</td>
+        <td>-</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -63,6 +218,7 @@ const cityData = {
   Zhejiang: ['Hangzhou', 'Ningbo', 'Wenzhou'],
   Jiangsu: ['Nanjing', 'Suzhou', 'Zhenjiang'],
 };
+const OPTIONS = ['Apples', 'Nails', 'Bananas', 'Helicopters'];
 export default {
   name: "App",
   components: {
@@ -70,11 +226,17 @@ export default {
   },
   created() {
   },
+  computed: {
+    filteredOptions() {
+      return OPTIONS.filter(o => !this.selectedItems.includes(o));
+    },
+  },
   data() {
     return {
       value: "value",
       checked: true,
       disabled: false,
+      selectedItems: [],
       selectValue: "Lucy2",
       selectValue2: "Lucy2",
       selectValue3: "jack",
@@ -89,6 +251,9 @@ export default {
       this.cities = cityData[value];
       this.secondCity = cityData[value][0];
       console.log(this.secondCity)
+    },
+    handleChange4(selectedItems) {
+      this.selectedItems = selectedItems;
     },
     handleChange(value) {
       console.log(`selected ${value}`);
