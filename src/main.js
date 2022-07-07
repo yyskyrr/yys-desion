94c95f9813496cceb1a24c344c2437819eb6b88a
lanche4 <1165697552@qq.com>
2022-07-07 19:56:40 +0800

    update: 7.7

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "vue-awesome/icons";
-import Icon from "vue-awesome/components/Icon";
 import "./index.scss";
-import "font-awesome/css/font-awesome.min.css";
 
-Vue.component("v-icon", Icon);
 Vue.config.productionTip = false;
 
 new Vue({
