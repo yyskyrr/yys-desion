47fface6c775e06ac0fce40b8768f6a6f1f98563
闫雅帅 <1165697552@qq.com>
2022-08-21 18:59:48 +0800

    update: 8.21

--- src/components/yys-select/yys-time-select.vue
+++ src/components/yys-select/yys-time-select.vue
@@ -1,38 +1,79 @@
 <template>
   <div :class="{ 'yys-select-disabled': disabled }" class="yys-select">
-    <div v-if="isTags"></div>
+    <div
+      class="yys-select-box"
+      @mouseenter="mouseenter"
+      @mouseleave="showClose = false"
+      @click="onClick"
+    >
+      <YInput
+        v-model="label"
+        :disabled="disabled"
+        :placeholder="newPlaceholder"
+        :size="size"
+        @blur="onblur"
+      >
+        <template #prefix>
+          <i
+            aria-hidden="true"
+            class="fa fa-clock-o"
+            style="color: #d9d9d9"
+          ></i>
+        </template>
+        <template #suffix>
+          <slot name="suffixIcon">
+            <i
+              v-show="showClose"
+              aria-hidden="true"
+              class="fa fa-times-circle"
+              style="cursor: pointer; color: #d9d9d9"
+              @click.stop="handleClear"
+            ></i>
+          </slot>
+        </template>
+      </YInput>
+    </div>
+    <div v-if="selectableRange">
+      <div v-show="isFocus" class="yys-time-range-box">
+        <div
+          v-for="(item, index) in optionsList"
+          v-if="optionsList.length > 0"
+          :key="index"
+          :class="{
+            'yys-option-selected': currentValue == item.value,
+            'yys-option-disabled': item.disabled,
+          }"
+          class="yys-option-item"
+          @mousedown="(e) => onSelect(item, e)"
+        >
+          {{ item.label }}
+        </div>
+        <div v-if="optionsList.length === 0" class="yys-empty-option">
+          <svg
+            height="41"
+            viewBox="0 0 64 41"
+            width="64"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fillRule="evenodd" transform="translate(0 1)">
+              <ellipse cx="32" cy="33" fill="#F5F5F5" rx="32" ry="7"></ellipse>
+              <g fillRule="nonzero" stroke="#D9D9D9">
+                <path
+                  d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"
+                ></path>
+                <path
+                  d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+                  fill="#FAFAFA"
+                ></path>
+              </g>
+            </g>
+          </svg>
+          {{ emptyText || "暂无数据" }}
+        </div>
+      </div>
+    </div>
 
     <div v-else>
-      <div
-        class="yys-select-box"
-        @mouseenter="mouseenter"
-        @mouseleave="showClose = false"
-        @click="onClick"
-      >
-        <YInput
-          v-model="label"
-          :disabled="disabled"
-          :placeholder="newPlaceholder"
-          :size="size"
-          @blur="onblur"
-        >
-          <template #prefix>
-            <slot name="prefixIcon"></slot>
-          </template>
-          <template #suffix>
-            <slot name="suffixIcon">
-              <i
-                v-show="showClose"
-                aria-hidden="true"
-                class="fa fa-times-circle"
-                style="cursor: pointer; color: #d9d9d9"
-                @click.stop="handleClear"
-              ></i>
-            </slot>
-          </template>
-        </YInput>
-      </div>
-
       <div v-show="isFocus" class="yys-option-box">
         <div
           v-for="(item, index) in optionsList"
@@ -82,11 +123,8 @@ export default {
   data() {
     return {
       isFocus: this.defaultOpen || this.open,
-      isTagsFocus: false,
       showClose: false,
       optionsList: [],
-      tagsList: [],
-      tagsListValue: [],
       newTimeSelectList: [],
       label: "",
       currentValue: this.value || this.defaultValue,
@@ -125,9 +163,6 @@ export default {
     },
   },
   computed: {
-    isTags() {
-      return this.mode === "tags" || this.mode === "multiple";
-    },
     timeSelectList() {
       const list = [];
       let time = this.start;
